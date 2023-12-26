4687775370bb6b2e9d87f636dd7cf046040ea573
Alokananda Y <plantsfortrees@gmail.com>
2023-12-26 18:48:43 +0530

    Fishing algorithm

--- ruby-tutorial/fishing-algorithm/lets_go_fishing.rb
+++ ruby-tutorial/fishing-algorithm/lets_go_fishing.rb
@@ -8,19 +8,18 @@ def go_fishing
 
     if underwater == "yes"
       puts "Hook and pull in fish."
+
+      puts "Done fishing? (yes/no)"
+      done_fishing = gets.chomp.downcase
+
+      if done_fishing == "yes"
+        puts "Great! Time to go home."
+        break
+      else
+        puts "Alright, let's go back to step 1."
+      end
     else
       puts "No fish yet. Keep waiting..."
-      next
-    end
-
-    puts "Done fishing? (yes/no)"
-    done_fishing = gets.chomp.downcase
-
-    if done_fishing == "yes"
-      puts "Great! Time to go home."
-      break
-    else
-      puts "Alright, let's go back to step 1."
     end
   end
 end
