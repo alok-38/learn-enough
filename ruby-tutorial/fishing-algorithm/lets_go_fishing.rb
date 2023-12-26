658d48ffeacb9e2cdcabdb8db5e14649e0606c92
Alokananda Y <plantsfortrees@gmail.com>
2023-12-26 15:03:37 +0530

    Fishing algorithm involving infinite loop

---
+++ ruby-tutorial/fishing-algorithm/lets_go_fishing.rb
@@ -0,0 +1,30 @@
+def go_fishing
+  puts "Put worm on hook."
+  puts "Cast line into pond."
+
+  loop do
+    puts "Watch the bobber until it goes underwater."
+    underwater = gets.chomp.downcase
+
+    if underwater == "yes"
+      puts "Hook and pull in fish."
+    else
+      puts "No fish yet. Keep waiting..."
+      next
+    end
+
+    puts "Done fishing? (yes/no)"
+    done_fishing = gets.chomp.downcase
+
+    if done_fishing == "yes"
+      puts "Great! Time to go home."
+      break
+    else
+      puts "Alright, let's go back to step 1."
+    end
+  end
+end
+
+# Start fishing
+go_fishing
+
