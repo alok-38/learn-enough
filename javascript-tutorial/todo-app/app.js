4e357193596c4eaa5328c505a7649c0d89e13e3f
Alokananda Y <plantsfortrees@gmail.com>
2024-01-14 09:05:32 +0530

    Todo app

---
+++ javascript-tutorial/todo-app/app.js
@@ -0,0 +1,29 @@
+class TodoApp {
+    constructor() {
+      this.tasks = [];
+      this.taskInput = document.getElementById('taskInput');
+      this.addTaskBtn = document.getElementById('addTaskBtn');
+      this.taskList = document.getElementById('taskList');
+  
+      this.addTaskBtn.addEventListener('click', () => this.addTask());
+    }
+  
+    addTask() {
+      const taskText = this.taskInput.value.trim();
+  
+      if (taskText !== '') {
+        const taskItem = document.createElement('li');
+        taskItem.textContent = taskText;
+  
+        this.tasks.push(taskText);
+        this.taskList.appendChild(taskItem);
+  
+        this.taskInput.value = '';
+      }
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    new TodoApp();
+  });
+  
