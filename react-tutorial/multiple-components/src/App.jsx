fde94a34461c9a74c264db87cb37ba33273ebc60
Alokananda Y <plantsfortrees@gmail.com>
2024-03-08 13:51:26 +0530

    props

---
+++ react-tutorial/multiple-components/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+    </div>
+  )
+}
+
+export default App
