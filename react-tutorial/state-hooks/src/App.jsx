4b34850190056ab4c7d8d3ce443997265163a320
Alokananda Y <plantsfortrees@gmail.com>
2024-03-12 16:07:15 +0530

    usestate

---
+++ react-tutorial/state-hooks/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function HomePage() {
+  const [likes, setLikes] = React.useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>Likes ({likes})</button>
+    </div>
+  );
+}
+
+export default HomePage
