6b936c3f81fe07935ceb01aafe0fab15691eef07
Alokananda Y <plantsfortrees@gmail.com>
2024-03-12 07:04:24 +0530

    DOM

--- dom-manipulation/dom-example/script.js
+++ dom-manipulation/dom-example/script.js
@@ -2,3 +2,17 @@ const linkEl = document.querySelector('a');
 linkEl.textContent = "Mozilla Developer Network";
 linkEl.href = "https://developer.mozilla.org";
 
+const sectionEl = document.querySelector('section');
+const paraEl = document.createElement('p');
+paraEl.textContent = 'We hope you enjoyed the ride!';
+
+sectionEl.appendChild(paraEl);
+
+const text = document.createTextNode(
+    " — the premier source for web development knowledge.",
+);
+
+const linkPara = document.querySelector("p");
+linkPara.appendChild(text);
+
+
