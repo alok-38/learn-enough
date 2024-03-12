e204f22117f010aa3dd454742f56c618d984f85e
Alokananda Y <plantsfortrees@gmail.com>
2024-03-12 11:30:18 +0530

    props

--- react-tutorial/data-props/src/App.jsx
+++ react-tutorial/data-props/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function createTitle(title="Alok") {
+function createTitle(title) {
   if (title) {
     return title;
   } else {
@@ -15,4 +15,13 @@ function Header({ title }) {
   return <h1>{createTitle(title)}</h1>;
 }
 
-export default Header
+function HomePage() {
+  return (
+    <div>
+      <Header title="WWE" />
+      <Header title="React" />
+    </div>
+  )
+}
+
+export default HomePage
