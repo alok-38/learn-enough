dc2139df009cf3c18efb02a1bcce13f1881ca180
Alokananda Y <plantsfortrees@gmail.com>
2024-03-28 20:18:40 +0530

    todo

---
+++ dave-gray/todo-list/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoApp from './components/TodoApp';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ dave-gray/todo-list/src/components/TodoApp.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+function TodoApp() {
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleAddTodo = () => {
+        if (inputValue.trim() !== '') {
+            setTodos([...todos, inputValue]);
+            setInputValue('');
+        }
+    };
+
+    const handleDeleteTodo = (index) => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+
+    return (
+        <div>
+            <h1>Todo List</h1>
+            <input
+                type="text"
+                placeholder="Enter a todo"
+                value={inputValue} // Ensure that inputValue is always defined
+                onChange={handleInputChange}
+            />
+            <button onClick={handleAddTodo}>Add</button>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>
+                        {todo}
+                        <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default TodoApp;
