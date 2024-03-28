80e6c74f9aea4551db91824d8d598d7154a02b7d
Alokananda Y <plantsfortrees@gmail.com>
2024-03-28 18:05:21 +0530

    click

---
+++ dave-gray/click-events/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../logo.svg';
+const Header = () => {
+    return (
+        <header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>React App</h1>
+        </header>
+    );
+}
+
+export default Header;
