1fe9e6bac4e58f9551db92407a50665fa5ceeece
Alokananda Y <plantsfortrees@gmail.com>
2024-03-28 17:53:40 +0530

    components

---
+++ dave-gray/styling-components/src/components/Header.js
@@ -0,0 +1,12 @@
+import logo from '../logo.svg';
+const Header = () => {
+
+    return (
+        <header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Groceries List</h1>
+        </header>
+    )
+}
+
+export default Header;
